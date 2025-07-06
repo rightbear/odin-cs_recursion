233073855b9028266f5425e1786297636ba6ad01
rightbear <jjj17041113@gmail.com>
2025-07-06 21:37:01 +0800

    feat: add file to tackle the fibonacci sequence

---
+++ fibonacci.js
@@ -0,0 +1,67 @@
+#!/usr/bin/env node
+
+// Iteration version of fibonacci function
+function fibsIter(index) {
+    let first = 0, second = 1, sum = 0;
+    const fibResult = [];
+
+    if(Number.isInteger(index)) {
+        if(index >= 1){
+            for(let i=1 ; i<=index ; i++){
+                if(i === 1){
+                    fibResult.push(first);
+                }
+                else if(i === 2){
+                    fibResult.push(second);
+                }
+                else{
+                    sum = first + second;
+                    first = second;
+                    second = sum;
+                    fibResult.push(sum);
+                }
+            }
+        }  
+    }
+    return fibResult;
+}
+
+// Recursion version of fibonacci function
+function fibsRec(index) {
+    if(Number.isInteger(index)) {
+        if(index <= 0) {
+            return [];
+        }
+        else {
+            console.log("This was printed recursively");
+            let result = fibsRec(index-1);
+
+            if(index === 1 || index === 2){
+                return result.concat([index-1])
+            }
+            else{
+                return result.concat([(result[result.length-1] + result[result.length-2])]);
+                // Can also write as following code
+                // return [...result, result[result.length-1] + result[result.length-2]]
+            }
+        }
+    }
+    else {
+        return [];
+    }
+}
+
+// Test iteration version of fibonacci function
+console.log(fibsIter(-1));        // []
+console.log(fibsIter(1.1));       // []
+console.log(fibsIter(8));         // [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibsIter(2));         // [0, 1]
+console.log(fibsIter(10));        // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+
+// Test recursion version of fibonacci function
+console.log(fibsIter(-1));        // []
+console.log(fibsIter(1.1));       // []
+console.log(fibsIter(8));         // [0, 1, 1, 2, 3, 5, 8, 13]
+console.log(fibsIter(2));         // [0, 1]
+console.log(fibsIter(10));        // [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
